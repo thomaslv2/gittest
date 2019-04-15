cb416c16f72b2b4fc46c72947f8c1c4b5090b922
lvxianweithomas <995255289@qq.com>
2019-04-15 20:41:11 +0800

    修改dev分支

--- gitweb/views.py
+++ gitweb/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,HttpResponse,redirect
 
 def index(request):
     print('stash0后')
+    print("master和dev第一次合并分之后，dev再修改")
     return HttpResponse('欢迎登陆首页')
 def register(request):
     print('增加dev分支')
