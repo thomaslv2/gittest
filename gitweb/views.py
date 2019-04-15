6491b1042c7bb7e097e30a2bc3f8cfa9d79459d6
lvxianweithomas <995255289@qq.com>
2019-04-15 20:21:14 +0800

    合并stash并开发register视图函数未完“

--- gitweb/views.py
+++ gitweb/views.py
@@ -3,4 +3,6 @@ from django.shortcuts import render,HttpResponse,redirect
 def index(request):
     print('stash0后')
     return HttpResponse('欢迎登陆首页')
+def register(request):
+    return HttpResponse('开发注册页面')
 
