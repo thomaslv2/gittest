a15bb5af7a3c0e982b55549161abd0816e19ad24
lvxianweithomas <995255289@qq.com>
2019-04-15 22:21:35 +0800

    处理githubdev和dev1的冲突

--- gitweb/views.py
+++ gitweb/views.py
@@ -6,8 +6,11 @@ def index(request):
     return HttpResponse('欢迎登陆首页')
 def register(request):
     print('增加dev分支')
+<<<<<<< HEAD
     print('gothub后dev第一次输入')
+=======
     print("创建bug分支")
     print('gothub后dev1第一次输入')
+>>>>>>> bd8770aeedb5a283e077824cae67742e4a8520ac
     return HttpResponse('开发注册页面')
 
