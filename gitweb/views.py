44977f992a7b97212f9852001bee83c679983480
lvxianweithomas <995255289@qq.com>
2019-04-15 20:28:25 +0800

    dev 分之后master分支继续修改“

--- gitweb/views.py
+++ gitweb/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,HttpResponse,redirect
 
 def index(request):
     print('stash0后')
+    print("创建dev分支后master同位置修改")
     return HttpResponse('欢迎登陆首页')
 def register(request):
     return HttpResponse('开发注册页面')
